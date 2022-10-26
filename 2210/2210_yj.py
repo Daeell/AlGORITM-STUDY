795c0258e77181f54b56a62ec77795c3452df1e2
yoojinLiz <yj.oh.biz@gmail.com>
2022-10-26 12:59:44 +0900

    유진 숫자판 수정 30840kb 80ms

--- 2210/2210_yj.py
+++ 2210/2210_yj.py
@@ -1,5 +1,4 @@
 import sys
-sys.stdin = open("input.txt","r")
 input = sys.stdin.readline
 
 nums = []
@@ -7,26 +6,25 @@ for i in range(5):
     temp = input().split()
     nums.append(temp)
 
-visited = set() #[] 리스트를 원소로 갖는 집합
-
+visited = set() 
 dx= [-1,1,0,0]
 dy= [0,0,-1,1]
-#x부터 시작하는 DFS를 구현, 단 visited는 6번째 숫자일 때만 기록
-current_visit= tuple()
-def dfs(x,y):
-    global visited, current_visit
-    current_visit.append(nums[x][y])
-    if len(current_visit)==6 :
-        visited.add(current_visit);
-        current_visit=[] # 초기화
+
+# nums[x][y]부터 시작하는 DFS를 구현
+def dfs(x,y, num):
+    global visited
+    num += nums[x][y]
+    if len(num)==6 :
+        visited.add(num);
         return 
     for i in range(4):
-        dfs(x+dx[i], y+dy[i])
+        if x+dx[i]>=0 and x+dx[i]<5 and y+dy[i]>=0 and y+dy[i]<5 :
+            dfs(x+dx[i], y+dy[i], num)
 
 for i in range(5):
     for j in range(5):
-        current_visit.append(nums[i][j])
-        dfs(i,j)
+        num = str()
+        dfs(i,j,num)
 
 print(len(visited))
     
