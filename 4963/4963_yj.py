79a6b02f5412517137bed33b4348d03e82530441
yoojinLiz <yj.oh.biz@gmail.com>
2022-10-28 14:53:53 +0900

    유진 heqpq bfs로 재시도 32492kb 124ms

--- 4963/4963_yj.py
+++ 4963/4963_yj.py
@@ -3,34 +3,38 @@ from collections import deque
 sys.stdin = open("input.txt","r")
 input = sys.stdin.readline
 
-
-def bfs(p,q, island) :
-    dx = [0,0,-1,-1,-1,1,1,1]
-    dy = [-1,1,-1,0,1,-1,0,1]
-
+def bfs(p,q, island, visited) :
+    dx = (0,0,-1,-1,-1,1,1,1)
+    dy = (-1,1,-1,0,1,-1,0,1)
     # p,q 방문처리
-    island.remove((p,q))
-    for i in range(8):
-        if p+dx >=0 and p+dx <w and q+dy>=0 and q+dy <h :
-
+    queue = deque()
+    queue.append((p,q))
+    visited.add((p,q))
+    while len(queue) != 0 :
+        p,q = queue.popleft()
+        island.remove((p,q)) 
+        for i in range(8):
+            if p+dx[i] >=0 and p+dx[i] <h and q+dy[i]>=0 and q+dy[i] <w :
+                if (p+dx[i], q+dy[i]) in island and (p+dx[i],q+dy[i]) not in visited :
+                    queue.append((p+dx[i],q+dy[i])) # 인큐
+                    visited.add((p+dx[i],q+dy[i]))
 
 def do_each(w,h) :
     mat = []
     island= set()
     for i in range(h):
         tmp = input().strip().split()
-        mat.append(input().strip().split())
-        for j in range(j) :
-            if tmp[i][j] == '1' : #섬이면 따로 섬의 집합을 만들어 둔다
+        mat.append(tmp)
+        for j in range(w) :
+            if tmp[j] == '1' : #섬이면 따로 섬의 집합을 만들어 둔다
                 island.add((i,j))
-    # 아무 섬(1)에서나 bfs 시행
-    # 방문할 때마다 island 목록에서 지우고, 모든 방문이 끝났을 때 방문하지 않은 섬이 있다면 섬의 갯수를 +1 하고 다시 bfs
     cnt = 0 #섬 갯수 
+    visited = set()
     while len(island) != 0 :
+        cnt += 1 
         for p,q in island :
-            bfs(p,q, island)
+            bfs(p,q, island, visited)
             break
-    
     print(cnt) #섬의 갯수 출력 
         
 while True: 
@@ -39,5 +43,3 @@ while True:
         break
     else :
         do_each(w,h)
-
-    
