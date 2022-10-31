67a964f5d4b82e94f289c8768fa87138ced183cc
ChiWoo-Shin <97394724+ChiWoo-Shin@users.noreply.github.com>
2022-10-31 13:24:36 +0900

    Update 20551_scw.py
    동일한 값을 찾는 index를 찾은 후 그 index를 기준으로 앞으로 같은 값을 찾으며 index를 줄이는 방법도 사용해보았으나
    해당 방법은 시간초과가 발생합니다.

--- 20551/20551_scw.py
+++ 20551/20551_scw.py
@@ -1,34 +1,41 @@
 # Sort 마스터 배지훈의 후계자   
 import sys
-
 input=sys.stdin.readline
 
-M,N = map(int,input().strip().split())
+N,M = map(int,input().strip().split())
+
+# def find_index(mid):
+#     while True:
+#         if A[mid-1]==data and mid-1>=0:
+#             mid=mid-1
+#         else:
+#             return mid
+
+def BST(data,s,e):
+    answer=0
+    while s<=e :
+        mid=(s+e)//2
+        if A[mid]>=data:
+            answer=mid
+            e=mid-1            
+        # elif A[mid] == data:
+        #     answer=find_index(mid)
+        #     break
+        else:
+            s=mid+1
+        
+    if A[answer] == data:
+        print(answer)
+    else:
+        print(-1)
 
 A=[]
 for i in range(N):
     A.append(int(input().strip()))
+
 A.sort()
 
-D=[]
 for i in range(M):
-    D.append(int(input().strip()))
-
-
-for data in D:
+    data=int(input().strip())
     s, e = 0, N-1
-    while s<e :
-        mid=(s+e)//2
-        if A[mid]>=data:
-            e=mid-1
-        else:
-            s=mid+1
-    
-    if e<0:
-        e=0
-        
-    if A[(s+e)//2] !=data:
-        print(-1)
-    else:
-        print((s+e)//2)
-
+    BST(data,s,e)
