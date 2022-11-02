6c5b085d148e329d86af217bcef332ce4f2d6277
Junho Moon <112556572+ProdMoon@users.noreply.github.com>
2022-11-02 13:35:51 +0900

    feat: 9657 solved (30840KB, 72ms)

--- 9657/9657_MJH.py
+++ 9657/9657_MJH.py
@@ -1,22 +1,14 @@
-def DFS(current_rock) -> int:
-    if DP[current_rock] != -1:
-        return DP[current_rock]
-    for i in range(3):
-        win_probability = 0
-        n = DFS(current_rock - pick_rock[i])
-        if n != 0:
-            win_probability = 1
-            DP[current_rock] = n
-    if win_probability == 0:
-        DP[current_rock] = 0
-
 N = int(input())
 DP = [-1]*(1001)
 DP[1] = 1
 DP[2] = 0
-DP[3] = 3
-DP[4] = 4
-pick_rock = (1, 3, 4)
-if DFS(N) == 0:
+DP[3] = 1
+DP[4] = 1
+
+if N > 4:
+    for i in range(5, N+1):
+        DP[i] = (min(DP[i-1], DP[i-3], DP[i-4])+1)%2
+
+if DP[N] == 0:
     print("CY")
 else: print("SK")
