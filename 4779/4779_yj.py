5ec7bdaf7ff24ec62ce386eed010e62a9669f5f0
yoojinLiz <yj.oh.biz@gmail.com>
2022-11-09 11:09:52 +0900

    유진 재귀 topdown 	31700	68

---
+++ 4779/4779_yj.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin= open("input.txt", "r")
+N= sys.stdin.readlines()
+
+def getAnswer(n) :
+    if (n == 0) :
+        return "-"
+    else : 
+        frontAndBack = getAnswer(n-1)
+        mid = " "* (3**(n-1))
+        return frontAndBack + mid + frontAndBack
+
+for i in range(len(N)) :
+    N[i] = int(N[i].strip())
+    print(getAnswer(N[i]))
