884d69e54002566dfd466b79115e63cdb9c4d20a
ChiWoo-Shin <shin8037@naver.com>
2022-11-21 13:30:00 +0900

    32412kb 88ms

--- 5397/5397_scw.py
+++ 5397/5397_scw.py
@@ -13,20 +13,21 @@ for _ in range(N):
     for i in A:
         if answer != None:
             if i == '<':
-                cnt -=1
-                if len(answer)==0:
+                if len(answer)==0 or cnt ==0:
                     cnt=0
+                else:
+                    cnt -=1
             elif i ==">":
                 cnt +=1
-                if len(answer)==0:
+                if len(answer)+cnt>=len(answer) or len(answer) ==0:
                     cnt=0
             elif i == "-":
                 if len(answer)+cnt<0:
-                    answer.popleft()
+                    continue
                 elif len(answer)+cnt>len(answer):
                     answer.pop()
                 else:
-                    del answer[len(answer)+cnt-1]
+                    del answer[len(answer)+cnt]
             else:
                 answer.insert(len(answer)+cnt,i)
     print(*answer,sep='')
