57a9d29828f8e0d549d796bad1969ffcb4959d23
ChiWoo-Shin <shin8037@naver.com>
2022-11-23 13:47:27 +0900

    45396kb 164ms

--- 5397/5397_scw.py
+++ 5397/5397_scw.py
@@ -5,30 +5,32 @@ input=sys.stdin.readline
 
 N=int(input().strip())
 
-for _ in range(N):
-    A=[]
-    A=list(input().strip())
-    cnt = 0
-    answer=deque()
-    for i in A:
-        if answer != None:
-            if i == '<':
-                if len(answer)==0 or cnt ==0:
-                    cnt=0
-                else:
-                    cnt -=1
-            elif i ==">":
-                cnt +=1
-                if len(answer)+cnt>=len(answer) or len(answer) ==0:
-                    cnt=0
-            elif i == "-":
-                if len(answer)+cnt<0:
-                    continue
-                elif len(answer)+cnt>len(answer):
-                    answer.pop()
-                else:
-                    del answer[len(answer)+cnt]
-            else:
-                answer.insert(len(answer)+cnt,i)
-    print(*answer,sep='')
+# for _ in range(N):
+#     A=[]
+#     A=list(input().strip())
+#     cnt = 0
+#     answer=deque()
+#     for i in A:
+#         if answer != None:
+#             if i == '<':
+#                 if len(answer)==0 or cnt ==len(answer)*(-1):
+#                     continue
+#                 else:
+#                     cnt -=1
+#             elif i ==">":
+#                 if len(answer) ==0 or cnt==0:
+#                     cnt=0
+#                 else:
+#                     cnt +=1
+#             elif i == "-":
+#                 if len(answer)+cnt<=0:
+#                     continue
+#                 elif cnt >= 0:
+#                     answer.pop()
+#                 else:
+#                     del answer[len(answer)+cnt-1]
+#             else:
+#                 answer.insert(len(answer)+cnt,i)
+#     print(*answer,sep='')
 
+for _ in range(N):
