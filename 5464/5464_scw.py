3bd711427cff8937ef2eea7c7aaca56204305a31
ChiWoo-Shin <shin8037@naver.com>
2022-11-30 14:31:24 +0900

    32460kb 120ms

--- 5464/5464_scw.py
+++ 5464/5464_scw.py
@@ -10,26 +10,26 @@ cars=[ int(input()) for _ in range(M)]
 visited = [0]*N
 answer=0
 dq=deque()
+wait=deque()
 for _ in range(2*M):
     dq.append(int(input().strip()))
 
-while(dq != None):
+while(len(dq)!=0 or len(wait) !=0):
     a=dq.popleft()
     if a>0:
         for i in range(N):
             if visited[i]==0:
-                visited[i]=cars[a-1]
+                visited[i]=a
                 answer +=parking[i]*cars[a-1]
                 break
-        if i==N-1:    
-            dq.append(a) 
+            if i==N-1:
+                wait.append(a)
     else:
-        for i in range(N):
-            if visited[i]==cars[a*(-1)-1]:
-                visited[i]=0
-                break
-    
+        i = visited.index(abs(a))
+        visited[i]=0
+        if len(wait) !=0:
+            b=wait.popleft()
+            visited[i]=b
+            answer+=parking[i]*cars[b-1]
 
 print(answer)
-                
-
