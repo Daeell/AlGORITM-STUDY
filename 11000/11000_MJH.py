9817d2eb31715862e8a2baa1d77e13d496b74143
Junho Moon <112556572+ProdMoon@users.noreply.github.com>
2022-12-02 15:33:40 +0900

    문준호 solved

--- 11000/11000_MJH.py
+++ 11000/11000_MJH.py
@@ -1,45 +1,20 @@
+from heapq import heappush, heappop
 import sys
 input = sys.stdin.readline
-
-def b_search(start, end, val, cmp_idx) -> int:
-    answer = -1
-    while start <= end:
-        mid = (start + end) // 2
-        if lecList[mid][cmp_idx] < val:
-            start = mid+1
-        else:
-            if lecList[mid][2] == 0:
-                answer = mid
-            end = mid-1
-
-    return answer
-    
 
 N = int(input())
 lecList = []
 for i in range(N):
-    lecList.append(list(map(int, input().split()))+[0])
+    lecList.append(list(map(int, input().split())))
 
-lecList.sort(key=lambda x: x[1])
-lecList.sort(key=lambda x: x[0])
+lecList.sort(key=lambda x: (x[0], x[1]))
 
-result = 0
-for i in range(N):
-    # If used, then continue
-    if lecList[i][2] == 1:
-        continue
+heap = []
+heappush(heap, lecList[0][1])
 
-    # Found unused lecture.
-    result += 1
-    lecList[i][2] = 1
-    cur = i
+for i in range(1, N):
+    if heap[0] <= lecList[i][0]:
+        heappop(heap)
+    heappush(heap, lecList[i][1])
 
-    while cur+1 < N:
-        idx = b_search(cur+1, N-1, lecList[cur][1], 0)
-        if idx == -1:
-            break
-        else:
-            lecList[idx][2] = 1
-            cur = idx
-
-print(result)
+print(len(heap))
