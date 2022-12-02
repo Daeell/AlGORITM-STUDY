4136d87c5fc3bb7b6053e1d1db667363b6e17aa4
Junho Moon <112556572+ProdMoon@users.noreply.github.com>
2022-12-02 17:03:08 +0900

    문준호 improved performance

--- 11000/11000_MJH.py
+++ 11000/11000_MJH.py
@@ -1,20 +1,31 @@
-from heapq import heappush, heappop
+# Improved performance.
+# 86236KB, 528ms -> 72144KB, 384ms
+#
+# Changed methods :
+#   - heappop & heappush -> heapreplace
+#   - sort by [0] and [1] -> only [0] (no sorting options)
+
+from heapq import heappush, heapreplace
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-lecList = []
-for i in range(N):
-    lecList.append(list(map(int, input().split())))
+def sol():
+    N = int(input())
+    lecList = []
+    for i in range(N):
+        lecList.append(list(map(int, input().split())))
 
-lecList.sort(key=lambda x: (x[0], x[1]))
+    lecList.sort()
 
-heap = []
-heappush(heap, lecList[0][1])
+    heap = []
+    heappush(heap, lecList[0][1])
 
-for i in range(1, N):
-    if heap[0] <= lecList[i][0]:
-        heappop(heap)
-    heappush(heap, lecList[i][1])
+    for i in range(1, N):
+        if heap[0] <= lecList[i][0]:
+            heapreplace(heap, lecList[i][1])
+        else:
+            heappush(heap, lecList[i][1])
+    
+    return len(heap)
 
-print(len(heap))
+print(sol())
