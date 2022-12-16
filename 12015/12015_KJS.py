0b4598adb7271c0380fd278ae86a7967c8600f8f
JacobKim0226 <110660688+JacobKim0226@users.noreply.github.com>
2022-12-16 15:25:42 +0900

    151976kb	1036ms

--- 12015/12015_KJS.py
+++ 12015/12015_KJS.py
@@ -1,22 +1,15 @@
-# import sys
-# N = int(sys.stdin.readline())
-# A = list(map(int, sys.stdin.readline().split()))
-
-# dp = [1] * N
-
-# for i in range(N):
-#     for j in range(0,i):
-#         if A[i] > A[j]:
-#             dp[i] = max(dp[i], dp[j]+1)
-
-# print(dp)
-
-
-
 import sys
+from bisect import bisect_left
 N = int(sys.stdin.readline())
 A = list(map(int, sys.stdin.readline().split()))
-emul_A = list(enumerate(A))
-emul_A = sorted(emul_A, key = lambda x : (x[1], x[0]))
-print(emul_A)
-# A_sort = sorted(A)
+
+letsfind = [0]
+
+for a in A:
+    if letsfind[-1] < a:
+        letsfind.append(a)
+    elif letsfind[-1] > a:
+        idx = bisect_left(letsfind,a)
+        letsfind[idx] = a
+
+print(len(letsfind)-1)
