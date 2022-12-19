b5ccb6239c9fb1a3498d7d220d9643f2ca047cd2
JacobKim0226 <110660688+JacobKim0226@users.noreply.github.com>
2022-12-19 16:18:45 +0900

    블로그 글 참고 하였습니다,

--- 1062/1062_KJS.py
+++ 1062/1062_KJS.py
@@ -1,14 +1,43 @@
+from itertools import combinations
 import sys
-import re
 N, K = map(int, sys.stdin.readline().split())
-words = []
-basic_word = ['a','n','t','i','c']
-for i in range(N):
-    words.append(list(set(re.sub(r"[a,n,t,i,c]","",(sys.stdin.readline().strip())))))
+answer = 0
 
-# print(words)
-can_teach = K-5
+default_word = {'a','n','i','c','t'}
 
+remain_alphbet = set(chr(i) for i in range(97,123)) - default_word
+data = [sys.stdin.readline().rstrip()[4:-4] for _ in range(N)]
 
-if K < 5:
+def countwords(data,can_use):
+    cnt = 0
+    for word in data:
+        can_read = 1
+        for w in word:
+            if can_use[ord(w)] == 0:
+                can_read = 0
+                break
+        
+        if can_read == 1:
+            cnt += 1
+    return cnt
+
+if K >=5:
+    # 아스키코드 사용하여 해당 알파벳의 사용 여부를 확인
+    can_use = [0] * 123
+    for x in default_word:
+        can_use[ord(x)] = 1  # 무조건 배워야하는 알파벳 아스키코드 인덱스 value를 1로 최신화
+
+    # default_alphabet 를 빼고 남은 모든 알파벳에서 K-5개를 선택하는 경우의수를 다 돌아보면서 answer를 최대값으로 최신화 시킨다
+    for teach in list(combinations(remain_alphbet,K-5)):
+        for t in teach:
+            can_use[ord(t)] = 1
+        cnt = countwords(data,can_use)
+
+        if cnt > answer:
+            answer = cnt
+        for t in teach:
+            can_use[ord(t)] = 0
+    
+    print(answer)
+else:
     print(0)
