240729164364eb2726435bdbc13885fa9c7f4ca2
Junho Moon <112556572+ProdMoon@users.noreply.github.com>
2022-12-21 18:23:14 +0900

    speedrun

--- 11728/11728_MJH.py
+++ 11728/11728_MJH.py
@@ -1,19 +1,6 @@
-A_LEN, B_LEN = map(int, input())
-A = list(map(int, input().split()))
-B = list(map(int, input().split()))
-arr = []
-
-def append_while_small_than_n (which_list, idx, n):
-    while idx < len(which_list):
-        if A[idx] <= n:
-            arr.append(A[idx])
-            idx += 1
-        else:
-            break
-    return idx
-
-a_idx = 0
-b_idx = 0
-if A[0] < B[0]:
-    a_idx = append_while_small_than_n (A, a_idx, B[b_idx])
-    b_idx = append_while_small_than_n (B, b_idx, A[a_idx])
+TRASH = input()
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+c = a + b
+c.sort()
+print(*c)
