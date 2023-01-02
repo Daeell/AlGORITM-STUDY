75daac1d24d74c6a46f1e46ce50e5936320f4af6
Junho Moon <112556572+ProdMoon@users.noreply.github.com>
2023-01-02 19:54:28 +0900

    문준호 제출(화가 많이 나는 문제네요)

--- 2339/2339_MJH.py
+++ 2339/2339_MJH.py
@@ -1,55 +1,83 @@
 import sys
-from collections import defaultdict
 input = sys.stdin.readline
 
-def check(area) -> bool:
-    pass
+def main(N):
 
+    def area_check(begin_x, end_x, begin_y, end_y):
+        imp_cnt = 0
+        jewel_cnt = 0
+        for i in range(begin_x, end_x+1):
+            for j in range(begin_y, end_y+1):
+                if area[i][j] == 1:
+                    imp_cnt += 1
+                elif area[i][j] == 2:
+                    jewel_cnt += 1
+        return imp_cnt, jewel_cnt
 
-def cut_row(idx:int, area_start:tuple, area_end:tuple) -> None:
-    global count, area, jewels_row, jewels_col, impurities_rowsort, impurities_colsort
-    imp = impurities_rowsort[idx]
-    r = imp[0]; c = imp[1]
-    for i in range(area_start[0], area_end[0]+1):
-        if area[r][i] == 2:
-            return
-    for i in range()
-    cut_col()
+    def cut_row(r, begin, end):
+        # If row is on the edge of the area, return 0.
+        if r == begin[0] or r == end[0]:
+            return 0
+        # If the jewel is on the row, return 0.
+        for i in range(begin[1], end[1]+1):
+            if area[r][i] == 2:
+                return 0
+        # Check the area.
+        above_imp, above_jwl = area_check(begin[0], r-1, begin[1], end[1])
+        below_imp, below_jwl = area_check(r+1, end[0], begin[1], end[1])
+        if not above_jwl or not below_jwl:
+            return 0
+        if above_jwl <= above_imp or below_jwl <= below_imp:
+            return 0
+        if above_jwl == 1 and below_jwl == 1 and above_imp == 0 and below_imp == 0:
+            return 1
+        else:
+            above_cnt = 1 if above_imp == 0 and above_jwl == 1 else 0
+            below_cnt = 1 if below_imp == 0 and below_jwl == 1 else 0
+            if above_jwl > 1:
+                for c in range(begin[1], end[1]+1):
+                    above_cnt += cut_col(c, begin, (r-1, end[1]))
+            if below_jwl > 1:
+                for c in range(begin[1], end[1]+1):
+                    below_cnt += cut_col(c, (r+1, begin[1]), end)
+            return above_cnt * below_cnt
 
+    def cut_col(c, begin, end):
+        # If column is on the edge of the area, return 0.
+        if c == begin[1] or c == end[1]:
+            return 0
+        # If the jewel is on the column, return 0.
+        for i in range(begin[0], end[0]+1):
+            if area[i][c] == 2:
+                return 0
+        # Check the area.
+        above_imp, above_jwl = area_check(begin[0], end[0], begin[1], c-1)
+        below_imp, below_jwl = area_check(begin[0], end[0], c+1, end[1])
+        if not above_jwl or not below_jwl:
+            return 0
+        if above_jwl <= above_imp or below_jwl <= below_imp:
+            return 0
+        if above_jwl == 1 and below_jwl == 1 and above_imp == 0 and below_imp == 0:
+            return 1
+        else:
+            above_cnt = 1 if above_imp == 0 and above_jwl == 1 else 0
+            below_cnt = 1 if below_imp == 0 and below_jwl == 1 else 0
+            if above_jwl > 1:
+                for r in range(begin[0], end[0]+1):
+                    above_cnt += cut_row(r, begin, (end[0], c-1))
+            if below_jwl > 1:
+                for r in range(begin[0], end[0]+1):
+                    below_cnt += cut_row(r, (begin[0], c+1), end)
+            return above_cnt * below_cnt
 
-def cut_col(imp:tuple, area_start:tuple, area_end:tuple) -> None:
-    global count, area, jewels_row, jewels_col, impurities_rowsort, impurities_colsort
-    
-
-def parse(area:list):
-    impurities = []
-    jewels_row = defaultdict(list)
-    jewels_col = defaultdict(list)
-    N = len(area)
-    for i in range(N):
-        for j in range(N):
-            if area[i][j] == 1:
-                impurities.append((i, j))
-            elif area[i][j] == 2:
-                jewels_row[i].append(j)
-                jewels_col[j].append(i)
-    return impurities, jewels_row, jewels_col
-
-
-def main(N:int) -> int:
-    global count, area, jewels_row, jewels_col, impurities_rowsort, impurities_colsort
-    count = 0
+    cnt = 0
     area = []
     for _ in range(N):
         area.append(list(map(int, input().split())))
-    impurities, jewels_row, jewels_col = parse(area)
-    impurities_rowsort = sorted(impurities)
-    impurities_colsort = sorted(impurities, lambda x: x[1])
-    for i in range(len(impurities_rowsort)):
-        cut_row(i, (0,0), (N-1,N-1))
-    for imp in impurities_colsort:
-        cut_col(imp, (0,0), (N-1,N-1))
-    return count
-
+    for r in range(N):
+        cnt += cut_row(r, (0,0), (N-1, N-1))
+    for c in range(N):
+        cnt += cut_col(c, (0,0), (N-1, N-1))
+    return cnt if cnt else -1
 
 print(main(int(input().strip())))
