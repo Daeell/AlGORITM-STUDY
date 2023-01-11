2bf27cdd0810f20e7d5fd12a3790197bfc0843ac
Junho Moon <112556572+ProdMoon@users.noreply.github.com>
2023-01-11 18:58:33 +0900

    어케 풀긴 했습니다... 문제가 잘못된건 아니었어요

--- programmers/150369/mjh.py
+++ programmers/150369/mjh.py
@@ -1,28 +1,22 @@
 def solution(cap, n, deliveries, pickups):
     answer = 0
-    def deliver():
-        while True:
-            if cur_cap - deliveries[cur_deli] >= 0:
-                cur_cap -= deliveries[cur_deli]
-                cur_deli -= 1
-            else:
-                deliveries[cur_deli] - cur_cap
-                break
-    def pickup():
-        while True:
-            if cur_cap - pickups[cur_pick] >= 0:
-                cur_cap -= pickups[cur_pick]
-                cur_pick -= 1
-            else:
-                pickups[cur_pick] - cur_cap
-                break
-    while cur_deli > 0 or cur_pick > 0:
-        if cur_deli > 0:
-            deliver()
-        if cur_pick > 0:
-            pickup()
-    cur_cap = cap
-    cur_deli = n-1
-    cur_pick = n-1
-    
+    for i in range(n-1, -1, -1):
+        if deliveries[i] != 0 or pickups[i] != 0:
+            break
+    p = i
+    if i <= 0:
+        return answer
+    answer += (p+1)*2
+    cap_deliver = 0
+    cap_pickup = 0
+    for i in range(p, -1, -1):
+        cap_deliver += deliveries[i]
+        cap_pickup += pickups[i]
+        while cap_deliver > cap or cap_pickup > cap:
+            answer += (i+1)*2
+            cap_deliver -= cap
+            cap_pickup -= cap
+
     return answer
+
+# print(solution(10,2,[0,50],[50,0]))
