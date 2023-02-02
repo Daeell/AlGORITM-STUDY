1be20561bf763c69d77b2978e04fa027e6e1fc1e
Yoojin Oh <95731504+yoojinLiz@users.noreply.github.com>
2023-02-02 12:13:05 +0900

    Create yj.py

---
+++ programmers/42842/yj.py
@@ -0,0 +1,32 @@
+import math
+
+def solution(brown, yellow):
+    
+    # 가로 길이가 w, 세로가 h일 때, brown = w*2 + h*2 -4, yellow = (w-2)(h-2)
+    
+    a = -1
+    b = 2+ brown/2
+    c = -brown -yellow
+
+    # 해가 2개인 경우
+    if b**2 - 4*a*c > 0:
+        x1 = (-b + math.sqrt(b**2 - 4*a*c)) / 2*a
+        x2 = (-b - math.sqrt(b**2 - 4*a*c)) / 2*a
+    
+    # 해가 1개인 경우
+    elif b**2 - 4*a*c == 0:
+        x1 = (-b + math.sqrt(b**2 - 4*a*c)) / 2*a
+        x2 = x1
+    
+
+    answer = [] #가로, 세로 
+    if x1>x2 : 
+        answer.append(round(x1))
+        answer.append(round(x2))
+    else:
+        answer.append(round(x2))
+        answer.append(round(x1))
+        
+    return answer
+
+print(solution(10, 2))
