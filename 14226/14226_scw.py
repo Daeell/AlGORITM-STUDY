410e77388282c88096e8e70a45cb886bb88df313
ChiWoo-Shin <shin8037@naver.com>
2023-02-04 22:41:26 +0900

    이모티콘 풀이 추가

--- 14226/14226_scw.py
+++ 14226/14226_scw.py
@@ -1,29 +1,36 @@
 #이모티콘
 import sys
+from collections import deque
 input=sys.stdin.readline
 
 N = int(input().strip())
 
-sec =0
-emo =1
-clip = 0
-temp_sec =0
+emo = 1
+clipboard = 0
+visited = dict()
+visited[(1, 0)] = 0
 
-def bfs ():
-    if N == emo:
-        sec = min(sec, temp_sec)
-        return sec
-    elif emo > N:
-        emo -=1
-        temp_sec +=1
-        bfs()
-    else:
-        if clip ==0:
-            clip = emo
-            temp_sec +=1
-            bfs()
-        else:
-            emo +=clip
-            temp_sec +=1
-            bfs()
+def bfs(emo, clipboard):
+    dq=deque()
+    dq.append((emo, clipboard))
 
+    while dq:
+        emo, clipboard = dq.popleft()
+        
+        if emo == N: # 이모티콘 개수에 도달했으면
+            return visited[(emo, clipboard)]
+        
+        if (emo, emo) not in visited.keys(): # 현재 이모티콘 개수를 clipboard에 복사
+            visited[(emo, emo)] = visited[(emo, clipboard)] + 1
+            dq.append((emo, emo))
+        
+        if (emo + clipboard, clipboard) not in visited.keys(): # clipboard에 있는 걸 화면으로 복사
+            visited[(emo + clipboard, clipboard)] = visited[(emo, clipboard)] +1
+            dq.append((emo + clipboard, clipboard))
+        
+        if (emo -1, clipboard) not in visited.keys(): # 이모티콘 개수 -1
+            visited[(emo -1, clipboard)] = visited[(emo, clipboard)] +1
+            dq.append((emo -1, clipboard))
+    
+
+print(bfs(emo, clipboard))
