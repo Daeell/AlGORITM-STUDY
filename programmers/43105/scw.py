2cdf701adbac867d827a34f7fc8f865e34dbb74a
ChiWoo-Shin <shin8037@naver.com>
2023-02-15 12:46:07 +0900

    dp 수정했어요

--- programmers/43105/scw.py
+++ programmers/43105/scw.py
@@ -1,20 +1,17 @@
 def solution(triangle):
-    dp = [0]*(len(triangle)+1)
-    print(triangle)
-    print(dp)
-    dp[0] = triangle[0][0]
-    dp[1] = max(triangle[1]) + dp[0]
+    dp = [[0]*i for i in range(1,len(triangle)+1)]
+    dp[0][0] = triangle[0][0]
 
-    if len(triangle) >=2:
-        for i in range(2, len(triangle)):
+    if len(triangle) >=1:
+        for i in range(1, len(triangle)):
             for j in range(len(triangle[i])):
                 if j ==0:
-                    dp[i]=max(triangle[i][j] + triangle[i-1][j] +dp[i-2], dp[i])
-                elif j ==len(triangle[i])-1:
-                    dp[i]=max(triangle[i][j] + triangle[i-1][j-1]+dp[i-2], dp[i]) 
+                    dp[i][j]=max(triangle[i][j] + dp[i-1][j], dp[i][j])
+                elif j ==i:
+                    dp[i][j]=max(triangle[i][j] + dp[i-1][j-1], dp[i][j])
                 else:
-                    dp[i]=max(triangle[i][j] + triangle[i-1][j]+dp[i-2],triangle[i][j] + triangle[i-1][j-1] +dp[i-2], dp[i])
-    answer = 0
+                    dp[i][j]=max(triangle[i][j] + dp[i-1][j],triangle[i][j] + dp[i-1][j-1], dp[i][j])
+    answer = max(dp[-1])
     return answer
 
 print(solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]))
