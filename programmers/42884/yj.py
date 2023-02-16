1161595b13853b02ec74b97eb3374c23101246c6
yoojinLiz <yj.oh.biz@gmail.com>
2023-02-16 12:03:49 +0900

    오래만에 풀었더니 정신 못차리겠네 ㅎ ㅠ ㅠ

---
+++ programmers/42884/yj.py
@@ -0,0 +1,13 @@
+
+def solution(routes):
+    # 출발위치를 기준으로 sort 
+    routes.sort()
+    ifPass = [0]*len(routes)
+     
+    for car in routes : 
+        car[0]
+        car[1]
+    answer = 0
+    return answer
+
+solution([[-20,-15], [-14,-5], [-18,-13], [-5,-3]])
