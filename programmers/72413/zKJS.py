3a58b4b18317b306f4c2538b9169a53e724ad57a
김지수 <110660688+JacobKim0226@users.noreply.github.com>
2023-02-19 13:27:13 +0900

    다익스트라도 가능

--- programmers/72413/zKJS.py
+++ programmers/72413/zKJS.py
@@ -1,16 +1,17 @@
 import heapq
 def solution(n, s, a, b, fares):
-    answer = 0
+    answer = 20000001
     INF = int(1e9)
     graph = [[] for _ in range(n+1)]
-    distance = [INF] * (n+1)
     for i in range(len(fares)):
         u, v, w = fares[i]
         graph[u].append((v,w))
         graph[v].append((u,w))
     
+    # dijkstra 함수는 각 노드에서 출발 했을 때 다른 노드로 갈 때의 최소값을 저장한 리스트를 반환
     def dijkstra(start):
         q = []
+        distance = [INF] * (n+1)
         heapq.heappush(q, (0,start))
         distance[start] = 0
         while q:
@@ -25,7 +26,15 @@ def solution(n, s, a, b, fares):
                 if cost < distance[i[0]]:
                     distance[i[0]] = cost
                     heapq.heappush(q,(cost,i[0]))
-    dijkstra(s)
-    print(distance[a],distance[b])
+        return distance
+    
+    all_case = [[]]
+    # 1번부터 n노드 까지 각 노드에서 출발 했을 때 각 노드에 도착할때까지의 최소비용 저장
+    for i in range(1,n+1):
+        all_case.append(dijkstra(i))
+    
+    # 출발 노드에서 각 노드까지 합승을 진행해보고 그때 택시 비용의 총합 중 최소값을 반환
+    for i in range(1,n+1):
+        answer = min(answer, all_case[s][i] + all_case[i][a] + all_case[i][b])
     
     return answer
