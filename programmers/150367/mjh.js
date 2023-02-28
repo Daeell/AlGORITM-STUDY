37eef3172966c0c19642902fb97bd443d82a719e
Junho Moon <112556572+ProdMoon@users.noreply.github.com>
2023-02-28 19:50:53 +0900

    문제가 이상해요

--- programmers/150367/mjh.js
+++ programmers/150367/mjh.js
@@ -1,35 +1,45 @@
-// RangeError: Maximum call stack size exceeded
-function binaryStringify (n) {
+// PASSED... 설명이 불친절한 문제...
+function binaryStringify(n) {
   let binString = n.toString(2);
-  if (binString.length % 2 == 0) {
-      binString = '0'+binString;
+  let i = 2;
+  while (i < binString.length+1) {
+    i *= 2;
+  }
+  while (binString.length < i-1) {
+    binString = "0" + binString;
   }
   return binString;
 }
 
-function checkBT (str) {
+function checkBT(str) {
   const checker = (left, right) => {
-      if (right-left === 0) {
-          return true;
-      }
-      let mid = (left+right)/2
-      if (str[mid] === '0') {
+    if (right - left === 0) {
+      return true;
+    }
+    let mid = (left + right) / 2;
+    if (str[mid] === "0") {
+      let i = left;
+      while (i <= right) {
+        if (str[i] === "1") {
           return false;
+        }
+        i++;
       }
-      if (checker(left, mid-1) && checker(mid+1, right)) {
-          return true;
-      }
-      return false;
-  }
-  return checker(0, str.length-1);
+      return true;
+    }
+    if (checker(left, mid - 1) && checker(mid + 1, right)) {
+      return true;
+    }
+    return false;
+  };
+  return checker(0, str.length - 1);
 }
 
 function solution(numbers) {
   let answer = [];
   numbers.map((n) => {
-      let binString = binaryStringify(n);
-      answer.push(+checkBT(binString));
-      console.log(binString);
-  })
+    let binString = binaryStringify(n);
+    answer.push(+checkBT(binString));
+  });
   return answer;
 }
