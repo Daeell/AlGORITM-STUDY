d0ea87f4276c5415f6bc7517774d2e2241d63e84
ChiWoo-Shin <shin8037@naver.com>
2023-02-28 22:59:08 +0900

    하 진짜.. 극악이네

--- programmers/150367/scw.js
+++ programmers/150367/scw.js
@@ -1,27 +1,38 @@
+function checkTree(b_str, start, end) {
+    const par = Math.floor((start+end)/2);
+    const left_c = Math.floor((start+ par -1)/2);
+    const right_c = Math.floor((par+1+end)/2);
+
+    if(start == end){
+        return true;
+    }
+
+    if (b_str[par] === '0' && ((b_str[left_c]==='1') || (b_str[right_c]==='1'))){
+        return false;
+    }
+
+    if (!checkTree(b_str, start, par-1)) return false;
+    if (!checkTree(b_str, par+1, end)) return false;
+    return true;
+}
+
 function solution(numbers) {
     var answer = [];
 
-    temp : function find_node(arr){
-        let mid = Math.ceil(arr.length/2);
-        console.log(arr)
-        if (arr[mid]===1){
-            let arr1 = arr.slice(0, mid-1);
-            let arr2 = arr.slice(mid,arr.length);
-            find_node(arr1)
-            find_node(arr2)
-        } else {
-            
-        }
-    }
-    
-    for(let i=0;i<numbers.length;i++){
-        let temp = numbers[i].toString(2);
-        if (temp.length%2 ===0){
-            temp = '0'+temp
-        }
-        
-    }
+    for (let num of numbers){
+        let bi = num.toString(2);
+        let bi_len = bi.length;
 
+        let m = bi_len.toString(2).length;
+        let bi_tree = '0'.repeat(2**m-1-bi_len);
+        bi_tree = bi_tree + '' + bi;
+
+        if(checkTree(bi_tree, 0, bi_tree.length -1)){
+            answer.push(1);
+        } else {
+            answer.push(0);
+        }
+    }
     return answer;
 }
 
