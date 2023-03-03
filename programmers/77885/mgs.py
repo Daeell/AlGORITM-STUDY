d897e30364c1e4dfe7f7011034c7557519a63fa6
Gyuseong Moon <47618270+ngngs@users.noreply.github.com>
2023-03-03 18:03:24 +0900

    푸렀다

--- programmers/77885/mgs.py
+++ programmers/77885/mgs.py
@@ -1,7 +1,38 @@
 def solution(numbers):
     answer = []
-    print(numbers)
     for num in numbers :
-        # while True :
-        print(list(bin(num)[2:]))
+        
+        if num == 1 :
+            answer.append(2)
+            continue
+        
+        bin_list = list(bin(num)[2:])
+        res = ""
+        
+        if bin_list.count("0") == 0 :
+            bin_list[1] = "0"
+            bin_list.append("1")
+            res = "".join(bin_list)
+            answer.append(int(res,2))
+            continue
+        
+        idx = 0
+        return_flag = False
+        while return_flag != True :
+            tmp = bin_list.pop()
+            if tmp == "0" :
+                bin_list.append("1")
+                return_flag = True
+            else :
+                idx += 1
+        
+        res = "".join(bin_list)
+        
+        if idx > 0 :
+            res += "0"
+            for i in range(idx-1) :
+                res += "1"
+        answer.append(int(res,2))
+                
+        
     return answer
