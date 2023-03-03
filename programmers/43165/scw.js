b843072bc54d9511c7a3a1c8ac43d766c1d31582
ChiWoo-Shin <shin8037@naver.com>
2023-03-03 20:30:50 +0900

    0303 올

--- programmers/43165/scw.js
+++ programmers/43165/scw.js
@@ -1,5 +1,25 @@
 function solution(numbers, target) {
     var answer = 0;
+
+    function find_answer(sum_, cnt) {
+        
+        if (cnt === numbers.length){
+            if(sum_ === target){
+                answer++;
+            }
+            return;
+        }
+        cnt++;
+        
+        find_answer(sum_+numbers[cnt-1], cnt);
+        find_answer(sum_-numbers[cnt-1], cnt);
+        
+        return;
+    }
+
+    find_answer(numbers[0], 1)
+    find_answer(-numbers[0], 1)
+
     return answer;
 }
 
