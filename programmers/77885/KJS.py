462717dcc96f574bf8eae5563670c17e977ca133
김지수 <110660688+JacobKim0226@users.noreply.github.com>
2023-03-03 15:21:20 +0900

    풀었슴당

--- programmers/77885/KJS.py
+++ programmers/77885/KJS.py
@@ -4,14 +4,10 @@ def solution(numbers):
         if num % 2 ==0:
             answer.append(num+1)
         else:
-            a = num % 8
-            if a == 1:
-                answer.append(num+1)
-            elif a == 3:
-                answer.append(num+2)
-            elif a == 5:
-                answer.append(num+1)
-            elif a == 7:
-                answer.append(num+4)
+            num_bin ='0'+bin(num)[2:]
+            for i in range(-1,-len(num_bin)-1,-1):
+                if num_bin[i] == '0':
+                    answer.append(num+2**(abs(i)-2))
+                    break
             
     return answer
