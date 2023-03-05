272104fc9329105df26d9dd2309e7a20c8f392fc
Gyuseong Moon <47618270+ngngs@users.noreply.github.com>
2023-03-05 11:51:45 +0900

    Update mgs.py

--- programmers/42746/mgs.py
+++ programmers/42746/mgs.py
@@ -1,16 +1,22 @@
-# 뭔가 정렬해놓고 붙히면 될 거 같은데 정렬을 어떻게 해야할지 모르겠다
-# 3보다 34는 크지만 32는 작게 정렬을 해야하는데 흠..
+# # 뭔가 정렬해놓고 붙히면 될 거 같은데 정렬을 어떻게 해야할지 모르겠다
+# # 3보다 34는 크지만 32는 작게 정렬을 해야하는데 흠..
+# def solution(numbers):
+#     answer = ''
+#     take = []
+#     store = []
+#     num = [str(tmp) for tmp in numbers]
+#     num = sorted(num, key = lambda x : (x[0], len(x)))
+#     num = [tmp.ljust(4, "A") for tmp in num]
+#     print(num)  
+    
+#     return answer
+
 def solution(numbers):
     answer = ''
-    take = []
-    store = []
     num = [str(tmp) for tmp in numbers]
-    num = sorted(num, key = lambda x : (x[0], len(x)))
-    num = [tmp.ljust(4, "A") for tmp in num]
+    num = sorted(num, key = lambda x : x*3, reverse=True)
+
     
-    
-    print(num)  
-    
-                
-    
-    return answer
+    answer = "".join(num)    
+ 
+    return answer if int(answer) != 0 else "0"
