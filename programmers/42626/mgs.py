3d9f78e04eeb6e902c1c8223eab7132fa06730a1
Gyuseong Moon <47618270+ngngs@users.noreply.github.com>
2023-03-07 11:13:38 +0900

    힙하다 힙해

---
+++ programmers/42626/mgs.py
@@ -0,0 +1,23 @@
+from heapq import *
+
+def solution(scoville, K):
+    answer = 0
+    sco = []
+    for val in scoville :
+        heappush(sco, val)
+    
+    tmp = heappop(sco)
+    if tmp >= K :
+        return 0
+    
+    while sco and tmp < K :
+        _next = heappop(sco)
+        tmp = tmp + (_next * 2)
+        heappush(sco, tmp)
+        answer += 1
+        tmp = heappop(sco)
+            
+    
+    return answer
+
+print(solution([1, 2, 3, 9, 10, 12], 7))
