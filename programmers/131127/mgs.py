cc38e2aba84ba6615cbc020b462dd7a9fa10ae2c
Gyuseong Moon <47618270+ngngs@users.noreply.github.com>
2023-03-10 15:44:20 +0900

    pass

--- programmers/131127/mgs.py
+++ programmers/131127/mgs.py
@@ -2,39 +2,34 @@ from collections import deque
 
 def solution(want, number, discount):
     answer = 0
-    _sum = sum(number)
     _dict = dict(zip(want, number))
     
-    tmp = dict()
-    _deque = deque()
-    for i in range(_sum) :
-        if discount[i] in tmp :
-            tmp[discount[i]] += 1
+    # 답지 만들어 놓기
+    for item in discount :
+        if item not in _dict :
+            _dict[item] = 0
+    
+    # 비교 답지 만들기
+    cpr_dict = dict()
+    for key in _dict.keys() :
+        cpr_dict[key] = 0
+    # print(cpr_dict)
+    check_list = []
+    discount = deque(discount)
+
+    while discount :
+        _next = discount.popleft()
+
+        if sum(cpr_dict.values()) < sum(number) :
+            cpr_dict[_next] += 1
+            check_list.append(_next)
         else :
-            tmp[discount[i]] = 1
-        _deque.append(discount[i])
-
-    if tmp == _dict :
-        answer += 1
-
-    cnt = 0
-    discount = discount[_sum:]
-
-    while cnt != len(discount)-1 :
-        _del = _deque.popleft()
-        tmp[_del] -= 1
-
+            cpr_dict[_next] += 1
+            cpr_dict[check_list[0]] -= 1
+            del check_list[0]
+            check_list.append(_next)
         
-        if discount[cnt] in tmp :
-            tmp[discount[cnt]] += 1
-        else :
-            tmp[discount[cnt]] = 1
-
-        if tmp == _dict :
+        if cpr_dict == _dict :
             answer += 1
-
-        cnt += 1
-
+            
     return answer
-
-solution(["apple"], [10], ["banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana"])
